4d2ff18df82464b9091d3e10f1bb7f37e869a172
James Swatton <jamessswatton@gmail.com>
2019-06-25 23:26:07 +0100

    initial commit

---
+++ src/main/java/com/codeclan/example/OneToManyREST/OneToManyRestApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.OneToManyREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToManyRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneToManyRestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/codeclan/example/OneToManyREST/models/File.java
@@ -0,0 +1,5 @@
+package com.codeclan.example.OneToManyREST.models;
+
+public class File {
+    
+}
